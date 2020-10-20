3af52ee2c9e7f5c564d1d9c66cb7ed7993332c0a
diogoBenevenuto <63623546+diogoBenevenuto@users.noreply.github.com>
2020-10-19 23:27:11 -0300

    Add files via upload

--- src/JogoDaVelha.java
+++ src/JogoDaVelha.java
@@ -9,18 +9,21 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 
 public class JogoDaVelha extends JFrame {
 	
 	ImageIcon iconCirculo = new ImageIcon(getClass().getResource("circulo.png"));
-	ImageIcon iconX = new ImageIcon(getClass().getResource("X.png"));
+	ImageIcon iconLetraX = new ImageIcon(getClass().getResource("LetraX.png"));
 	
 	JPanel pTela = new JPanel(new GridLayout(3, 3, 10, 10));
 	
 	
 	Bloco[] blocos = new Bloco[9];
+	
+	int rodadas = 0;
 	
 	final int JOGADOR_1 = 1;
 	final int JOGADOR_2 = 2;
@@ -50,6 +53,48 @@ public class JogoDaVelha extends JFrame {
 		}
 	}
 
+	public void mudarVez () {
+		if(jogadorVez==1) {
+			jogadorVez=2;
+			lInformacao.setText("Jogador 2");
+			lInformacao.setForeground(Color.RED);
+		} else {
+			jogadorVez=1;
+			lInformacao.setText("Jogador 1");
+			lInformacao.setForeground(new Color(50,200,50));
+		}
+		
+	}
+	
+	public boolean testarVitoria(int jogador) {
+		if(blocos[0].quem==jogador && blocos[1].quem==jogador && blocos[2].quem==jogador ) {
+			return true;
+		}
+		if(blocos[3].quem==jogador && blocos[4].quem==jogador && blocos[5].quem==jogador ) {
+			return true;
+		}
+		if(blocos[6].quem==jogador && blocos[7].quem==jogador && blocos[8].quem==jogador ) {
+			return true;
+		}
+		if(blocos[0].quem==jogador && blocos[3].quem==jogador && blocos[6].quem==jogador ) {
+			return true;
+		}
+		if(blocos[1].quem==jogador && blocos[4].quem==jogador && blocos[7].quem==jogador ) {
+			return true;
+		}
+		if(blocos[2].quem==jogador && blocos[5].quem==jogador && blocos[8].quem==jogador ) {
+			return true;
+		}
+		if(blocos[0].quem==jogador && blocos[4].quem==jogador && blocos[8].quem==jogador ) {
+			return true;
+		}
+		if(blocos[2].quem==jogador && blocos[4].quem==jogador && blocos[6].quem==jogador ) {
+			return true;
+		}
+			return false;
+	}
+	
+	
 	public void configurarJanela() {
 		setTitle("Jogo da Velha");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -74,9 +119,20 @@ public class JogoDaVelha extends JFrame {
 							setIcon(iconCirculo);
 							quem = JOGADOR_1;
 						} else {
-							setIcon(iconX);
+							setIcon(iconLetraX);
 							quem = JOGADOR_2;
 						}
+						if(testarVitoria(quem)) {
+							JOptionPane.showMessageDialog(null,"Jogador "+quem+" Venceu!");
+							System.exit(0);
+						}
+						rodadas++;
+						if(rodadas==9) {
+							JOptionPane.showMessageDialog(null,"Ninguem Venceu!");
+							System.exit(0);
+							
+						}
+						mudarVez();
 					}
 				});
 				
